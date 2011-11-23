0ba837af15e7139d78c95a973795bfba466175cf
laz <laz@2b13fa5e-b809-0410-b1a3-83a4c729f18f>
2011-11-23 15:27:48 +0000

    Add dependency ##

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(name='alloch.skin',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'plone.app.theming',
           # -*- Extra requirements: -*-
       ],
       entry_points="""
