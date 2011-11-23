fef706627bd2d060148cbbab4be42ca88a4ed731
laz <laz@2b13fa5e-b809-0410-b1a3-83a4c729f18f>
2011-11-23 14:27:41 +0000

    First alloch.skin import ##

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='alloch.skin',
+      version=version,
+      description="Allo CH skin",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Affinitic Sprl',
+      author_email='laurent.lasudry@affinitic.be',
+      url='http://svn.affinitic.be/plone/gites/alloch.skin',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['alloch'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
