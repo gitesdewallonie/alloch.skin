33bc416bb1dbc06503c18b7e657784a99a722a84
laz <laz@2b13fa5e-b809-0410-b1a3-83a4c729f18f>
2012-01-13 13:32:03 +0000

    Add QR code ##

--- alloch/skin/browser/store.py
+++ alloch/skin/browser/store.py
@@ -8,5 +8,8 @@ class Store(BrowserView):
     """
 
     def redirectQRcode(self):
+        """
+        QR Code points to http://www.allochambredhotes.be/download/
+        """
         self.request.response.redirect("http://itunes.apple.com/be/app/woodya-run/id302170850?mt=8")
         return ''
